aab36d14d45dd79bf09cbaaa70a00e83977dce72
engageintellect <jc9361@gmail.com>
2023-05-27 23:59:06 -0700

    working gql backend/client

---
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -0,0 +1,36 @@
+import { getClient } from "@/apollo-client";
+import fetchWeatherQuery from "@/graphql/queries/fetchWeatherQueries";
+
+type Props = {
+  params: {
+    city: string;
+    lat: string;
+    long: string;
+  };
+};
+
+async function WeatherPage({ params: { city, lat, long } }: Props) {
+  const client = getClient();
+
+  const { data } = await client.query({
+    query: fetchWeatherQuery,
+    variables: {
+      current_weather: "true",
+      longitude: long,
+      latitude: lat,
+      timezone: "America/Los_Angeles",
+    },
+  });
+
+  const results: Root = data.myQuery;
+
+  console.log(results);
+
+  return (
+    <div>
+      Welcome to weather page: {city} {lat} {long}
+    </div>
+  );
+}
+
+export default WeatherPage;
