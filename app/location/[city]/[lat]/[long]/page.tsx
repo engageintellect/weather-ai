36ec3327f378eb0adb010912361b999fc4b38eb7
engageintellect <jc9361@gmail.com>
2023-05-28 23:17:24 -0700

    init no gpt api

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -34,20 +34,20 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
 
   const results: Root = data.myQuery;
 
-  const dataToSend = cleanData(results, city);
+  // const dataToSend = cleanData(results, city);
 
-  const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      weatherData: dataToSend,
-    }),
-  });
+  // const res = await fetch(`${getBasePath()}/api/getWeatherSummary`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     weatherData: dataToSend,
+  //   }),
+  // });
 
-  const GPTdata = await res.json();
-  const { content } = GPTdata;
+  // const GPTdata = await res.json();
+  // const { content } = GPTdata;
 
   return (
     <div className="flex flex-col min-h-screen md:flex-row">
@@ -63,9 +63,9 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
               {results.timezone}
             </p>
           </div>
-          <div className="m-2 mb-10">
-            <CalloutCard message={content} />
-          </div>
+          {/* <div className="m-2 mb-10"> */}
+          {/* <CalloutCard message={content} /> */}
+          {/* </div> */}
 
           <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
             <StatCard
